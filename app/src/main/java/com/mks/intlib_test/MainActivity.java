09d59d9da6aed20ea91bcad57ad004f329f310e7
Khokhlov Artem <a.khokhlov@elari.net>
2019-02-19 17:30:25 +0300

    - тест промежуточной версии
    - Добавлена кнопка отправки сообщений в кибану
    - Обновлена библиотека на промежуточную версию для шведов

--- app/src/main/java/com/mks/intlib_test/MainActivity.java
+++ app/src/main/java/com/mks/intlib_test/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.mks.intlib.NotificationViewerGetter;
 import com.mks.intlib.REST.RestServicer;
@@ -23,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
       new NotificationViewerGetter().getNotificationViewer(getApplicationContext()).subscribeToTopic(getApplicationContext(), "install");
        // new NotificationViewerGetter().getNotificationViewer(getApplicationContext()).unsubscribeFromTopic(getApplicationContext(), "install");
 
+        Button btn = findViewById(R.id.btn_sendStat);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new RestServicer().send(getApplicationContext(), true, getSupportLoaderManager(), "Button click", "test app Button click");
+            }
+        });
     }
 
 
