3ab885cdffacea6c953c2ec3f5693ab304781822
Khokhlov Artem <a.khokhlov@elari.net>
2019-02-19 16:09:07 +0300

    ---

--- app/src/main/java/com/mks/intlib_test/MainActivity.java
+++ app/src/main/java/com/mks/intlib_test/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mks.intlib_test;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.mks.intlib.NotificationViewerGetter;
 import com.mks.intlib.REST.RestServicer;
@@ -12,8 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new NotificationViewerGetter().getNotificationViewer(getApplicationContext()).init(getApplicationContext(), "publisherId", "Gaid", "appid", "sdkversion");
-111
+        new NotificationViewerGetter().getNotificationViewer(getApplicationContext()).init(getApplicationContext(), "publisherId", "Gaid", "appid", "sdkversion", "com.mks.delete_me,sdasd.sdds.sss");
+
+
         new RestServicer().send(getApplicationContext(), true, getSupportLoaderManager(), "Start app", "test app start");
+
+      new NotificationViewerGetter().getNotificationViewer(getApplicationContext()).subscribeToTopic(getApplicationContext(), "install");
+       // new NotificationViewerGetter().getNotificationViewer(getApplicationContext()).unsubscribeFromTopic(getApplicationContext(), "install");
+
     }
+
+
 }
